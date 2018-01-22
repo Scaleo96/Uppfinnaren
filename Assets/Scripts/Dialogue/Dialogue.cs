c53a9f467cf8120957e7c58ffb807db80c6bfc1d
Johan Hallberg <scaleo96@hotmail.com>
2018-01-22 19:31:11 +0100

    Added Dialogue Class
    A scriptableobject containing an array with two elements; a string/line of dialogue and a speaker/entity of the character

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue System/Dialogue")]
+public class Dialogue : ScriptableObject {
+    public DialogueElements[] dialogue;
+}
+[System.Serializable]
+public class DialogueElements
+{
+   public Entity speaker;
+   public string line;
+}
