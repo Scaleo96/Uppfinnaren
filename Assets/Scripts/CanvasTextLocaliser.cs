1ec825721bfa9b7e6c75844bfb7fbee2e7b1e25d
Aksel Jensen <aksel.jensen@kroax.net>
2018-02-09 15:00:24 +0100

    Started localisation support for the menu

---
+++ Assets/Scripts/CanvasTextLocaliser.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasTextLocaliser : MonoBehaviour {
+
+    [SerializeField]
+    string englishText, swedishText = "";
+
+    Text canvasText;
+
+    private void Start()
+    {
+        // Get and store the Text component on this GameObject
+        canvasText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        switch (GlobalStatics.Language)
+        {
+            case ELanguage.English:
+                canvasText.text = englishText;
+                break;
+            case ELanguage.Swedish:
+                canvasText.text = swedishText;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
