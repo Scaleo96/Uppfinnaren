13a6d8c36dcfd3eb53983b5ba84ea57341318d6c
Aksel Jensen <aksel.jensen@kroax.net>
2018-02-09 11:31:01 +0100

    GlobalStatics - So far only used for selecting and checking language

---
+++ Assets/Scripts/GlobalStatics.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class GlobalStatics
+{
+    const ELanguage DEFAULT_LANGUAGE = ELanguage.Swedish;
+
+    /// <summary>
+    /// What localisation/language to use for text
+    /// </summary>
+    private static ELanguage language;
+
+    /// <summary>
+    /// What localisation/language to use for text
+    /// </summary>
+    public static ELanguage Language
+    {
+        get
+        {
+            switch (language)
+            {
+                case ELanguage.English:
+                    return language;
+
+                case ELanguage.Swedish:
+                    return language;
+
+                default:
+                    return DEFAULT_LANGUAGE;
+            }
+        }
+
+        set
+        {
+            language = value;
+        }
+    }
+}
+
+public enum ELanguage
+{
+    English,
+    Swedish
+}
