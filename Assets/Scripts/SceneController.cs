1d639770f1f3d6e8a54b3602fc0cd17d89ebbc3d
Marcus Karåker <marcus.karaker.mailbox@gmail.com>
2018-02-16 11:20:43 +0100

    SceneController

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,10 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 { // Ok Janne...
 
-    
+    [Tooltip("All the scenes that will be loaded.")]
+    [SerializeField]
+    Scene[] scenes;
 
+    private void Awake()
+    {
+        LoadScenes();
+    }
+
+    /// <summary>
+    /// Loads all scenes ontop of eachother and destroys the SceneController.
+    /// </summary>
+    private void LoadScenes()
+    {
+        foreach (Scene scene in scenes)
+        {
+            SceneManager.LoadScene(scene.buildIndex, LoadSceneMode.Additive);
+        }
+
+        Destroy(gameObject);
+    }
 }
