c9068cf82915190cab5f240d65770aeda38b1706
Aksel Jensen <aksel.jensen@kroax.net>
2018-02-27 14:43:42 +0100

    Attempt to show loading screen (needs more work)

---
+++ Assets/Scripts/SimpleSpriteAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSpriteAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Vector3 rotationVector = Vector3.zero;
+
+    private void Update()
+    {
+        rotationVector.z = rotationSpeed * Time.unscaledDeltaTime;
+        transform.Rotate(rotationVector);
+    }
+
+    private void Start()
+    {
+        //RotateObject();
+        
+    }
+
+    IEnumerator RotateObject()
+    {
+        while (false)
+        {
+            rotationVector.z = rotationSpeed * Time.time;
+            transform.Rotate(rotationVector);
+        }
+
+        yield return null;
+    }
+
+}
