509eca373d769233809d07b79645fb3b71c937c6
Johan Hallberg <35491445+Scaleo96@users.noreply.github.com>
2018-03-02 10:47:05 +0100

    Dialogue fix

---
+++ Assets/Scripts/Dialogue/DialogueControllerDeligate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueControllerDeligate : MonoBehaviour
+{
+
+    DialogueController dialogueController;
+
+    // Use this for initialization
+    void Start()
+    {
+        dialogueController = GameObject.Find("DialogueManager").GetComponent<DialogueController>();
+    }
+    public void RunDialogue(EntityValues entityValues)
+    {
+        dialogueController.RunDialogue(entityValues);
+    }
+}
